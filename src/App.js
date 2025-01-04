06bff871083e3e151c92c2282ea2af2438dec621
vonkensington <vonkensington@gmail.com>
2025-01-03 23:00:05 -0600

    Added AI page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import JobDetailsPage from './pages/JobDetailsPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import Navbar from './components/NavBar';
+import AiPage from './pages/AIPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/community" element={<CommunityPage />} />
+        <Route path="/AI" element={<AiPage isPaidUser={true} />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/jobs/:jobId" element={<JobDetailsPage />} />
         <Route path="/login" element={<LoginPage />} />
